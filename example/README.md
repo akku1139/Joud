e2d9a386a9d97a6fb07a8b82f785fa6cbad7c6b3
-akku- <151517253+akku1139@users.noreply.github.com>
2024-01-25 09:58:55 +0000

    なんか適当

---
+++ example/README.md
@@ -0,0 +1,3 @@
+# Joud example
+
+https://zenn.dev/zenn/articles/zenn-cli-guide
---
+++ example/joud.config.ts
@@ -0,0 +1,7 @@
+//import joud from "joud";
+//import joud from "../src/index.ts";
+
+import type { joud } from "joud";
+
+export default {
+} satisfies joud;
